08b42d4cc16d7e20054eeba8754a8cfcfb40a4f9
Mazedias <mawemw5@gmail.com>
2024-10-19 16:32:40 +0200

    Added function to read structure

--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -1,1 +1,2 @@
 pub mod updater;
+pub mod file_structure;
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,57 @@
 #[macro_use] extern crate rocket;
 
-use rocket::fs::{FileServer, relative};
+use file_manager::{file_structure::generate_file_structure, updater::{update_files, UpdateStatus}};
+use rocket::{fs::{relative, FileServer}, State};
+use rocket::serde::json::Json;
 use rocket_dyn_templates::{Template, context};
 use pulldown_cmark::{Parser, Options, html};
-use std::fs;
+use serde_json::{json, Value};
+use tokio::sync::RwLock;
+use std::{env, fs, sync::Arc, time::Duration};
 
 mod file_manager;
 
-#[get("/")]
-fn index() -> Template {
-    let files = fs::read_dir("content")
-        .unwrap()
-        .filter_map(|entry| {
-            entry.ok().and_then(|e|
-                e.path().file_stem()
-                    .and_then(|n| n.to_str().map(String::from))   
-            )
-        })
-        .collect::<Vec<String>>();
+#[derive(Clone)]
+struct UpdateState {
+    is_updating: bool,
+}
 
-    Template::render("base", context! { files: files, content: get_markdown("TheArchive") })
+impl UpdateState {
+    fn new() -> Self {
+        UpdateState { is_updating: false }
+    }
+}
+
+type SharedUpdateState = Arc<RwLock<UpdateState>>;
+
+
+#[get("/")]
+async fn index(update_state: &State<SharedUpdateState>) -> Template {
+    let is_updating = update_state.read().await.is_updating;
+
+    if is_updating {
+        return Template::render("base", context! {
+            content: get_markdown("messages/Updating"),
+        })
+    }
+
+    let file_structure = match generate_file_structure("content/repo") {
+        Ok(value) => value,
+        Err(_) => json!(""),
+    };
+
+    Template::render("base", context! { files: file_structure, content: get_markdown("messages/TheArchive") })
+}
+
+
+#[get("/file_structure")]
+fn get_file_structure() -> Json<Value> {
+    let json = match generate_file_structure("content/repo") {
+        Ok(value) => value,
+        Err(_) => json!({ "error": "Could not generate file structure!" })
+    };
+
+    Json(json)
 }
 
 
@@ -40,10 +72,48 @@ fn get_markdown(file: &str) -> String {
 }
 
 
+async fn update_files_periodically(update_state: SharedUpdateState) {
+    loop {
+        tokio::time::sleep(Duration::from_secs(3600)).await;
+
+        {
+            let mut state = update_state.write().await;
+            state.is_updating = true;
+        }
+
+        match update_files() {
+            UpdateStatus::SuccessfullUpdated => println!("Files updated successfully"),
+            UpdateStatus::NoUpdateNecessary => println!("Checked for update but was not neccessary"),
+            UpdateStatus::UpdateNotPossible => println!("Tried to update but couldn't"),
+        }
+
+        {
+            let mut state = update_state.write().await;
+            state.is_updating = false;
+        }
+    }
+}
+
+
 #[launch]
 fn rocket() -> _ {
+    let update_state = Arc::new(RwLock::new(UpdateState::new()));
+    let update_state_clone = update_state.clone();
+
+    let repo_is_set = match env::var("GIT_REPO_URL") {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
+    if repo_is_set {
+        tokio::spawn(async move {
+            update_files_periodically(update_state_clone).await;
+        });
+    }
+
     rocket::build()
-        .mount("/", routes![index, get_markdown])
+        .mount("/", routes![index, get_markdown, get_file_structure])
         .mount("/static", FileServer::from(relative!("static")))
         .attach(Template::fairing())
+        .manage(update_state)
 }
